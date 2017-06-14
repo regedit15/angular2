de738530d1e00ff16b013d7b89c872e782c6839c
regedit15 <001regedit15@gmail.com>
2017-06-14 14:30:44 -0300

    se crea el component timer

---
+++ prueba/src/app/timer/timer.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.css']
+})
+export class TimerComponent implements OnInit {
+
+ 
+
+  minutos: number;
+  segundos: number;
+  isPaused: boolean;
+  buttonLabel: string;
+
+  constructor() {
+    this.resetTimer();
+    setInterval(() => this.tick(), 1000);
+  }
+
+  ngOnInit() {
+  }
+
+  resetTimer(): void {
+    this.isPaused = true;
+    this.minutos = 24;
+    this.segundos = 59;
+    this.buttonLabel = 'Empezar';
+  }
+
+  private tick(): void {
+    if (!this.isPaused) {
+      this.buttonLabel = 'Pausar';
+
+      if (--this.segundos < 0) {
+        this.segundos = 59;
+        if (--this.minutos < 0) {
+          this.resetTimer();
+        }
+      }
+    }
+  }
+
+  togglePause(): void {
+    this.isPaused = !this.isPaused;
+    if (this.minutos < 24 || this.segundos < 59) {
+      this.buttonLabel = this.isPaused ? 'Reanudar' : 'Pausar';
+    }
+  }
+
+}
