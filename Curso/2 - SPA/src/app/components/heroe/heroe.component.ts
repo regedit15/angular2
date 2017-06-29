985d31501091a589cd53ac3d5d5291fc423ba7fe
regedit15 <001regedit15@gmail.com>
2017-06-28 22:54:57 -0300

    se realiza la pagina de heroe

--- Curso/2 - SPA/src/app/components/heroe/heroe.component.ts
+++ Curso/2 - SPA/src/app/components/heroe/heroe.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
 
 @Component({
-  selector: 'app-heroe',
-  templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.css']
+	selector: 'app-heroe',
+	templateUrl: './heroe.component.html',
+	styleUrls: ['./heroe.component.css']
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent {
 
-  constructor() { }
+    heroe: any = {};
 
-  ngOnInit() {
-  }
+	constructor(private activatedRoute: ActivatedRoute, private _heroesService: HeroesService) {
+		this.activatedRoute.params.subscribe(params => {
+            this.heroe = this._heroesService.getHeroe(params['id']);
+            console.log(this.heroe);
+        });
+	}
+
 
 }
