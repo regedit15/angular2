fee33f018d03ac022493ed53920d368250f01cc8
regedit15 <001regedit15@gmail.com>
2017-07-05 20:06:41 -0300

    se continua con la clase de pipes

--- Curso/3 - Pipes/src/app/app.component.ts
+++ Curso/3 - Pipes/src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre = 'Frenando';
-  lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  PI = Math.PI;
+	nombre = 'Frenando';
+	lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	PI = Math.PI;
+	a: number = 0.234;
+	salario: number = 1234.5;
+
+	persona = {
+		nombre: 'wolverine',
+		apellido: 'rodiguez',
+		edad: 30,
+		direccion: {
+			calle: 'cordoba',
+			numero: 15
+		}
+	};
+
+	valorDePromesa = new Promise(
+		(resolve, reject) => {
+			setTimeout(() => resolve('llego la data!'), 3500);
+		});
 }
