78a4c413ed452aae731188591ec8e96d7ee32432
regedit15 <001regedit15@gmail.com>
2017-07-10 17:17:38 -0300

    se crea proyecto de spotiapp

--- Curso/3 - Pipes/src/app/app.component.ts
+++ Curso/3 - Pipes/src/app/app.component.ts
@@ -1,29 +1,33 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	nombre = 'Frenando';
-	lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-	PI = Math.PI;
-	a: number = 0.234;
-	salario: number = 1234.5;
+  nombre = 'Frenando';
+  lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI = Math.PI;
+  a: number = 0.234;
+  salario: number = 1234.5;
 
-	persona = {
-		nombre: 'wolverine',
-		apellido: 'rodiguez',
-		edad: 30,
-		direccion: {
-			calle: 'cordoba',
-			numero: 15
-		}
-	};
+  persona = {
+    nombre: 'wolverine',
+    apellido: 'rodiguez',
+    edad: 30,
+    direccion: {
+      calle: 'cordoba',
+      numero: 15
+    }
+  };
 
-	valorDePromesa = new Promise(
-		(resolve, reject) => {
-			setTimeout(() => resolve('llego la data!'), 3500);
-		});
+  valorDePromesa = new Promise(
+    (resolve, reject) => {
+      setTimeout(() => resolve('llego la data!'), 3500);
+    });
+
+  video: string = "https://www.youtube.com/embed/H9QLTaanmNo?list=PLCKuOXG0bPi2WNLzimZB7GRSTgqj0J132"
+
+
 }
