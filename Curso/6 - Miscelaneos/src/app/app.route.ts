6cf9a4df9690a70beff2271677288a58fde52857
regedit15 <001regedit15@gmail.com>
2017-09-06 17:30:13 -0300

    se crean rutas hijas

--- Curso/6 - Miscelaneos/src/app/app.route.ts
+++ Curso/6 - Miscelaneos/src/app/app.route.ts
@@ -1,10 +1,16 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { USUARIO_ROUTES } from './components/usuario/usuario.route';
+
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'usuario', component: UsuarioComponent },
+  {
+    path: 'usuario/:id',
+    component: UsuarioComponent,
+    children: USUARIO_ROUTES
+  },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
