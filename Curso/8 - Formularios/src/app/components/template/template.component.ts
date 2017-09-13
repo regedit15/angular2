ec21939325f8cf55426d27694de0fef189879e45
regedit15 <001regedit15@gmail.com>
2017-09-13 12:16:49 -0300

    se termina con la parte del template

--- Curso/8 - Formularios/src/app/components/template/template.component.ts
+++ Curso/8 - Formularios/src/app/components/template/template.component.ts
@@ -8,16 +8,45 @@ import { NgForm } from '@angular/forms';
         .ng-invalid.ng-touched:not(form){
             border: 1px solid red;
         }
-        `
-    ]
+    `]
 })
 export class TemplateComponent implements OnInit {
 
     usuario: Object = {
         nombre: null,
         apellido: null,
-        correo: null
+        correo: null,
+        pais: "CRI",
+        sexo: "M",
+        color: "R",
+        acepta: false,
     };
+
+    paises = [{
+        codigo: "CRI",
+        nombre: "Costa Rica"
+    },
+    {
+        codigo: "AR",
+        nombre: "Argentina"
+    },
+    {
+        codigo: "EU",
+        nombre: "Estados Unidos"
+    }];
+
+    colores = [{
+        codigo: "R",
+        nombre: "Rojo"
+    },
+    {
+        codigo: "V",
+        nombre: "Verde"
+    },
+    {
+        codigo: "A",
+        nombre: "Azul"
+    }];
 
     constructor() { }
 
