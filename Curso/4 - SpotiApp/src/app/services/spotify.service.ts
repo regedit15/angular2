a6adfcc1b2dbd67811631fe222a5c6ec22f7bbb1
regedit15 <regedit15@hotmail.com>
2017-10-11 12:15:00 -0300

    se modifica la app

--- Curso/4 - SpotiApp/src/app/services/spotify.service.ts
+++ Curso/4 - SpotiApp/src/app/services/spotify.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -8,9 +8,9 @@ export class SpotifyService {
     //BQCCmRUoS-vPqoDkCTj4NQ5DOsG2UT2DXRoh20Ou2MCVB07pBkuPTvyekaZSY55d2SGD8FYeZWXbQcBviNOdSA
     artistas: any[];
 
-    urlBusqueda: string = "https://api.spotify.com/v1/search";
-    urlArtista: string = "https://api.spotify.com/v1/artists";
-    token: string = "BQBJLFB0VqYgH9-n6Gx3H3FMSpWIF_CQqV25yTouKoJpw7pcgFxxAAy-fqStGY8W2CW47cnygCzqQ0jJFzaXVg";
+    urlBusqueda: string = 'https://api.spotify.com/v1/search';
+    urlArtista: string = 'https://api.spotify.com/v1/artists';
+    token: string = 'BQC7i8WmoVj6Q_XHGHa26Q1OR5ZFt9SK8dJ6uCOReVobc40g_WP3SJ01mVZYMqDTw-_PQr8KIFM3Bydds-TV9A';
 
     // private http: Http
     constructor(private http: Http) {
@@ -23,7 +23,7 @@ export class SpotifyService {
         let query = `?q=${termino}&type=artist`;
         let url = this.urlBusqueda + query;
 
-        return this.http.get(url, { headers }).map(res => {
+        return this.http.get(url, {headers}).map(res => {
             this.artistas = res.json().artists.items;
         });
     }
@@ -35,7 +35,7 @@ export class SpotifyService {
         let query = `/${id}`;
         let url = this.urlArtista + query;
 
-        return this.http.get(url, { headers }).map(res => {
+        return this.http.get(url, {headers}).map(res => {
             console.log(res.json());
             return res.json();
         });
@@ -48,7 +48,7 @@ export class SpotifyService {
         let query = `/${id}/top-tracks?country=US`;
         let url = this.urlArtista + query;
 
-        return this.http.get(url, { headers }).map(res => {
+        return this.http.get(url, {headers}).map(res => {
             console.log(res.json().tracks);
             return res.json().tracks;
         });
